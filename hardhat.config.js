1d3fd3a930caf742a6d51cbd46b39116c871bfe7
danstevemukulupi <danstevemukulupi15@hotmail.com>
2026-04-23 08:59:20 +0100

    deploying process

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(); // added 1
 
 module.exports = {
   paths: {
@@ -14,4 +15,11 @@ module.exports = {
       viaIR: true
     }
   },
+
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL, // added 2
+      accounts: [process.env.PRIVATE_KEY] // added 3
+    }, // added4
+  }, // added 5
 };
